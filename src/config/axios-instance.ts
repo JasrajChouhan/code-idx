2ea9e1048eec2a8cf50587a4f7fd24bc6391469d
JasrajChouhan <jasrajc945@gmail.com>
2024-12-04 12:43:05 +0530

    (fix) : bug in get env file

--- src/config/axios-instance.ts
+++ src/config/axios-instance.ts
@@ -3,7 +3,7 @@ import { getEnv } from './getEnv';
 
 export const axiosInstance = axios.create({
   baseURL:
-    getEnv('NODE_ENV', 'production') === 'production'
-      ? getEnv('BACKEND_URL')
+    getEnv('VITE_NODE_ENV', 'production') === 'production'
+      ? getEnv('VITE_BACKEND_URL')
       : 'http://localhost:3000',
 });
--- src/config/getEnv.ts
+++ src/config/getEnv.ts
@@ -1,26 +1,33 @@
 import { z } from 'zod';
 
 export const environmentSchema = z.object({
-  NODE_ENV: z.enum(['production', 'development']),
-  BACKEND_URL: z.string().url(),
+  VITE_NODE_ENV: z.enum(['production', 'development']),
+  VITE_BACKEND_URL: z.string().url(),
 });
 
 export type Environment = z.infer<typeof environmentSchema>;
 
+const parsedEnv = environmentSchema.safeParse(import.meta.env);
+
+
+if (!parsedEnv.success) {
+  console.error('Invalid environment variables:', parsedEnv.error.format());
+  throw new Error('Environment validation failed.');
+}
+
+const validatedEnv = parsedEnv.data;
 export const getEnv = <K extends keyof Environment>(
   key: K,
   fallback?: Environment[K],
 ): Environment[K] => {
-  const rawValue = import.meta.env[key] as unknown;
-  const parsed = environmentSchema.safeParse({ [key]: rawValue });
 
-  if (!parsed.success || parsed.data[key] === undefined) {
+  const value = validatedEnv[key];
+  if (value === undefined) {
     if (fallback !== undefined) {
       return fallback;
     } else {
-      throw new Error(`Missing or invalid environment variable: ${key}.`);
+      throw new Error(`Missing environment variable: ${key}`);
     }
   }
-
-  return parsed.data[key];
+  return value;
 };
