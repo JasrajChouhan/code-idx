92932ffaba61472b75c604ab04bc524cad87666d
JasrajChouhan <jasrajc945@gmail.com>
2024-12-06 04:24:55 +0530

    (fix):   routing

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,9 +1,30 @@
-import { App } from '../App';
 import { createBrowserRouter } from 'react-router';
+import { Home } from '../pages/home';
+import { About } from '../pages/about';
+import { Services } from '../pages/services';
+import { Contact } from '../pages/contact';
+import { Dashboard } from '../pages/dashboard';
+import { Navbar } from '../components/molecules/navbar';
+import { ProtectedRoute } from '../protected-router';
 
+// Define the router
 export const router = createBrowserRouter([
   {
-    element: <App />,
     path: '/',
+    element: <Navbar />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/about', element: <About /> },
+      { path: '/services', element: <Services /> },
+      { path: '/contact', element: <Contact /> },
+      {
+        path: '/dashboard',
+        element: (
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        ),
+      },
+    ],
   },
 ]);
