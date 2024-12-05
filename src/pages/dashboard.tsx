9a5edc7e19e88c73ba07ef81ac7c52a9af7908aa
JasrajChouhan <jasrajc945@gmail.com>
2024-12-06 04:46:46 +0530

    (chore): showing user data on dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,15 @@
+import { useAuthStore } from '../store';
+
 export const Dashboard: React.FC = () => {
+  const { user } = useAuthStore();
   return (
     <div>
       <h1>Dashboard</h1>
       <p>Welcome to the protected Dashboard page!</p>
+      <h1>{user?.userId}</h1>
+      <h2>{user?.username}</h2>
+      <h1>{user?.userId}</h1>
+      <h1>{user?.email}</h1>
     </div>
   );
 };
