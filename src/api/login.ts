4c1943a8a477dc1a2a853e30aac28a4066588d0b
JasrajChouhan <jasrajc945@gmail.com>
2024-12-05 17:33:41 +0530

    (feat): persist data when user login

--- src/api/login.ts
+++ src/api/login.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 import ErrorBoundary from 'antd/es/alert/ErrorBoundary';
 import { axiosInstance } from '../config/axios-instance';
 import { LoginSchema } from '../schemas';
+import { useAuthStore } from '../store';
 
 export const login = async (formData: z.infer<typeof LoginSchema>) => {
   try {
@@ -10,6 +11,16 @@ export const login = async (formData: z.infer<typeof LoginSchema>) => {
       formData,
       { withCredentials: true },
     );
+    const { user: userData } = response.data.data;
+
+    // set the user data into zustand auth store
+    useAuthStore.getState().setUser({
+      userId: userData._id,
+      username: userData.username,
+      userRole: userData.userRole,
+      email: userData.email,
+      avatar: userData.avatar || '',
+    });
     return response.data;
   } catch (error) {
     throw new ErrorBoundary(error as Error);
