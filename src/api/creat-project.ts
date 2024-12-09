57290f218399be42cef1a3067df797a7c324449b
JasrajChouhan <jasrajc945@gmail.com>
2024-12-10 03:50:49 +0530

    (feat): add project in local data stroage

--- src/api/creat-project.ts
+++ src/api/creat-project.ts
@@ -2,19 +2,33 @@ import ErrorBoundary from 'antd/es/alert/ErrorBoundary';
 import { axiosInstance } from '../config/axios-instance';
 import { z } from 'zod';
 import { createProjectSchema } from '../schemas';
+import { useProjectStore } from '../store/project.store';
 
 export const createProject = async (
   createProjectData: z.infer<typeof createProjectSchema>,
 ) => {
   // currently run we a particulare docker container for user
-
   try {
     const response = await axiosInstance.post(
       '/api/v1/projects',
       createProjectData,
       { withCredentials: true },
     );
-    console.log(response.data);
+    console.log(response.data.data);
+    const { data: projectData } = response.data;
+
+    // Add the project to zustand store (project.store.ts)
+    const { addProject } = useProjectStore.getState();
+    addProject({
+      projectName: projectData?.projectName,
+      createdAt: projectData?.createdAt,
+      projectTechStack: projectData?.projectTechStack,
+      _id: projectData?._id,
+      authorId: projectData.author,
+      isActive: projectData.isActive,
+      updatedAt: projectData.updatedAt,
+      description: projectData.description,
+    });
     return response.data;
   } catch (error) {
     throw new ErrorBoundary(error as Error);
