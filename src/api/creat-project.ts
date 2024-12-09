e1ae1097dd403921252b58ccda4110cb98590af6
JasrajChouhan <jasrajc945@gmail.com>
2024-12-09 22:45:20 +0530

    (fix): create project api in frontend

--- src/api/creat-project.ts
+++ src/api/creat-project.ts
@@ -1,14 +1,17 @@
 import ErrorBoundary from 'antd/es/alert/ErrorBoundary';
 import { axiosInstance } from '../config/axios-instance';
+import { z } from 'zod';
+import { createProjectSchema } from '../schemas';
 
-export const createProject = async () => {
-  // we only focus on create react project on server
-  // But in future we change
+export const createProject = async (
+  createProjectData: z.infer<typeof createProjectSchema>,
+) => {
+  // currently run we a particulare docker container for user
 
   try {
     const response = await axiosInstance.post(
       '/api/v1/projects',
-      {},
+      createProjectData,
       { withCredentials: true },
     );
     console.log(response.data);
